e562226537bba78063bbea18f9eb607740f37f18
cleophass <95467960+cleophass@users.noreply.github.com>
2022-11-25 17:11:49 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello world</h1>
+    <!-- creer un bouton qui lie vers un autre fichier -->
+    <a href="test-con.php">lien vers caca</a>
+
+    
+</body>
+</html>
---
+++ test-con.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2>Connexion to database state: </h2>
+
+    <?php
+    ini_set('display_errors', '1');
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $db = "pet's_database";
+    $connexion = mysqli_connect($host, $user, $password, $db);
+
+    if (!$connexion) {
+        die("Connection failed: " . mysqli_connect_error());
+    } else {
+        echo "Connected successfully";
+    }
+
+    ?>
+</body>
+
+</html>
