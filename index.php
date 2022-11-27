ac100df4fa6ad2b57932b4dd60e9ac06839b1e8a
cleophass <95467960+cleophass@users.noreply.github.com>
2022-11-27 17:48:27 +0800

    MODIF PAGE ACCEUIL

--- index.php
+++ index.php
@@ -7,17 +7,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="styles/index.css">
+    <link rel="stylesheet" href="styles/nav.css">
+    
+    <link rel="stylesheet" href="styles/home.css">
+
 </head>
 
 <body>
-    <h1>Hello world</h1>
-    <!-- creer un bouton qui lie vers un autre fichier -->
-    <a href="test-con.php">lien vers test co</a>
-    <a href="registration.php">lien vers reg</a>
-    <!-- a href -->
-    <a href="login.php">connection</a>
+<div class="nav">
+        <!-- ADD AN IMAGE -->
+        
+        
+            <img src="assets/logo.png" alt="logo" height="100px" width="100px" class="interdit">
+            <!-- add space -->
+            <div class="space"></div>
+            
+        <ul id="item">
+            <li><a href="index.html"><p class="p1">Home Page   </p></a></li>
+            <li><a href="AboutMe.html"><p class="p2">About Me   </i></a></li>
+            <li><a href="db.php"><p class="p5">test <col>  </p></a></li>
+            <li><a href="buy.php"><p class="p3">BUY</p></a></li>
+            <li><a href="login.php"><img src="assets/login.png" alt="logo" height="100px" width="100px" class="image"></a></li>
+            
+            
 
-    <a href="buy.php">lien vers buy</a>
+           
+            
+
+        </ul>
+    </div>
+    <header>
+        <!-- section pour le header -->
+        <section id="header">
+        <h4>New season collection</h4>
+        <h2>Super value deals</h2>
+        <h1>On all products !</h1>
+        <p><i>70% discount until the 2nd of june! *</i></p>
+        <br>
+        <button class="button"><a href="#product">See products   <i class="fas fa-box"></a></i></button>
+        </section>
+    </header>
+
+    <div>
+
+
+    
+    </div>
+
+
+
+
+
 
 
 
