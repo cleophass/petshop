3d1e8c4a8ff0888e9a110a0846d040fd7af289e8
jeandtx <jean.doutriaux@gmail.com>
2022-11-28 12:16:52 +0800

    added images to animals

--- animals.php
+++ animals.php
@@ -26,6 +26,7 @@ ini_set('display_errors', '1');
     <table>
         <tr>
             <th>Name</th>
+            <th>Picture</th>
             <th>Species</th>
             <th>Race</th>
             <th>Weight</th>
@@ -37,6 +38,15 @@ ini_set('display_errors', '1');
         <?php foreach ($animals as $animal) : ?>
             <tr>
                 <td><?php echo $animal['name'] ?></td>
+                <td>
+                    <img src="<?php
+                                $api_url = 'https://dog.ceo/api/breeds/image/random';
+                                $json_data = file_get_contents($api_url);
+                                $response_data = json_decode($json_data);
+                                $user_data = $response_data->message;
+                                echo $user_data;
+                                ?>" alt="" width=" 100px">
+                </td>
                 <td><?php echo $animal['species'] ?></td>
                 <td><?php echo $animal['race'] ?></td>
                 <td><?php echo $animal['weight'] ?></td>
@@ -55,6 +65,7 @@ ini_set('display_errors', '1');
     <?php
     if (isset($_POST['buy'])) {
         $_SESSION['petId'] = $_POST['id'];
+        $_SESSION['bought'] = false;
     }
     ?>
     <a href="index.php">BAKHOME</a>
