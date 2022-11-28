146060c56da1f57191fe0a85e6260a622b83f5b6
jeandtx <jean.doutriaux@gmail.com>
2022-11-28 12:25:26 +0800

    refactor index

--- index.php
+++ index.php
@@ -8,58 +8,55 @@
     <title>Document</title>
     <link rel="stylesheet" href="styles/index.css">
     <link rel="stylesheet" href="styles/nav.css">
-    
+
     <link rel="stylesheet" href="styles/home.css">
 
 </head>
 
+<?php
+require_once 'db.php';
+ini_set('display_errors', '1');
+?>
+
 <body>
-<div class="nav">
-        <!-- ADD AN IMAGE -->
-        
-        
-            <img src="assets/logo.png" alt="logo" height="100px" width="100px" class="interdit">
-            <!-- add space -->
-            <div class="space"></div>
-            
+    <div class="nav">
+        <img src="assets/logo.png" alt="logo" height="100px" width="100px" class="interdit">
+        <div class="space"></div>
+
         <ul id="item">
-            <li><a href="index.html"><p class="p1">Home Page   </p></a></li>
-            <li><a href="AboutMe.html"><p class="p2">About Me   </i></a></li>
-            <li><a href="db.php"><p class="p5">test <col>  </p></a></li>
-            <li><a href="buy.php"><p class="p3">BUY</p></a></li>
-            <li><a href="login.php"><img src="assets/login.png" alt="logo" height="100px" width="100px" class="image"></a></li>
-            
-            
-
-           
-            
-
+            <li><a href="index.php">
+                    <p class="p1">Home Page </p>
+                </a></li>
+            <li><a href="AboutMe.html">
+                    <p class="p2">About Me </i>
+                </a></li>
+            <li><a href="db.php">
+                    <p class="p5">test
+                        <col>
+                    </p>
+                </a></li>
+            <li><a href="buy.php">
+                    <p class="p3">BUY</p>
+                </a></li>
+            <li><a href="login.php"><img src="assets/login.png
+            " alt="logo" height="100px" width="100px" class="image"></a></li>
         </ul>
     </div>
     <header>
-        <!-- section pour le header -->
         <section id="header">
-        <h4>New season collection</h4>
-        <h2>Super value deals</h2>
-        <h1>On all products !</h1>
-        <p><i>70% discount until the 2nd of june! *</i></p>
-        <br>
-        <button class="button"><a href="#product">See products   <i class="fas fa-box"></a></i></button>
+            <h4>New season collection</h4>
+            <h2>Super value deals</h2>
+            <h1>On all products !</h1>
+            <p><em>70% discount until the 2nd of june! *</em></p>
+            <br>
+            <button class="button"><a href="#animals">See products <em class="fas fa-box"></a></em></button>
         </section>
     </header>
-
-    <div>
-
-
-    
-    </div>
-
-
-
-
-
-
-
+    <animals id="animals">
+        <?php
+        require 'animals.php';
+        ?>
+    </animals>
 
 </body>
 
