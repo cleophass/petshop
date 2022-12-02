006f80eda1be47be5dd289d1bb7e43f3cbd956dc
jeandtx <jean.doutriaux@gmail.com>
2022-12-02 17:44:07 +0800

    error

--- SQL/insertpets.sql
+++ SQL/insertpets.sql
@@ -1,12 +1,12 @@
 use datapets;
 
-insert into pets values (NULL, 'Milo', 'Dog', 'Berger Allemand', 20, 2, 1000, 'Male', "https://pixabay.com/photos/dog-animal-black-mouth-roar-tooth-407078/", NULL);
-insert into pets values (NULL, 'Patty', 'Dog', 'Bouvier Bernois', 30, 4, 2000, 'Female', "https://pixabay.com/photos/nature-dog-greyhound-saluki-pet-3047194/", NULL);
-insert into pets values (NULL, 'Bella', 'Dog', 'Chihuahua', 5, 1, 500, 'Female', "https://pixabay.com/photos/dalmatian-dog-park-meadow-grass-6240485/", NULL);
-insert into pets values (NULL, 'Tiger', 'Cat', 'Siamois', 5, 2, 500, 'Male', "https://pixabay.com/photos/dog-pet-animal-cute-1839808/", NULL);
-insert into pets values (NULL, 'Luna', 'Cat', 'Siamois', 5, 2, 500, 'Female', "https://pixabay.com/photos/rottweiler-puppy-dog-dogs-cute-1785760/", NULL);
-insert into pets values (NULL, 'Nala', 'Monkey', 'baboon', 34.0, 2, 8700, 'female', "https://pixabay.com/photos/dog-animal-portrait-pet-brown-3277416/", NULL);
-insert into pets values (NULL, 'Coco', 'Monkey', 'baboon', 34.0, 2, 5000, 'male', "https://pixabay.com/photos/labrador-retriever-dog-pet-1210559/", NULL);
-insert into pets values (NULL, 'Lola', 'Monkey', 'baboon', 34.0, 2, 7000, 'female', "https://pixabay.com/photos/dog-pointer-pet-canine-animal-2702644/", NULL);
-insert into pets values (NULL, 'Felix', 'Tiger', 'malaysia', 105.3, 7, 80000, 'male', "https://pixabay.com/photos/dog-puppy-pet-black-dog-animal-423398/", NULL);
-insert into pets values (NULL, 'Leo', 'Tiger', 'malaysia', 105.3, 5, 100000, 'male', "https://pixabay.com/photos/pug-dog-blanket-dog-face-801826/", NULL);
+insert into pets values (NULL, 'Milo', 'Chien', 'Berger Allemand', 20, 2, 1000, 'Male', "https://images.dog.ceo/breeds/african/n02116738_2988.jpg", NULL);
+insert into pets values (NULL, 'Patty', 'Chien', 'Bouvier Bernois', 30, 4, 2000, 'Female', "https://www.sciencesetavenir.fr/assets/img/2022/05/11/cover-r4x3w1000-627b878098579-specificites-du-bouvier-bernois.jpg", NULL);
+insert into pets values (NULL, 'Bella', 'Chien', 'Chihuahua', 5, 1, 500, 'Female', "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Chihuahua_portrait.jpg/1200px-Chihuahua_portrait.jpg", NULL);
+insert into pets values (NULL, 'Tiger', 'Chat', 'Siamois', 5, 2, 500, 'Male', "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJvW9-B_nFFW8ifJ40fSIeVGl5YH4Mxu_b9g&usqp=CAU", NULL);
+insert into pets values (NULL, 'Luna', 'Chat', 'Siamois', 5, 2, 500, 'Female', "https://images.dog.ceo/breeds/african/n02116738_2988.jpg", NULL);
+insert into pets values (NULL, 'Nala', 'monkey', 'baboon', 34.0, 2, 8700, 'female', "https://images.dog.ceo/breeds/african/n02116738_2988.jpg", NULL);
+insert into pets values (NULL, 'Coco', 'monkey', 'baboon', 34.0, 2, 5000, 'male', "https://images.dog.ceo/breeds/african/n02116738_2988.jpg", NULL);
+insert into pets values (NULL, 'Lola', 'monkey', 'baboon', 34.0, 2, 7000, 'female', "https://images.dog.ceo/breeds/african/n02116738_2988.jpg", NULL);
+insert into pets values (NULL, 'Felix', 'tiger', 'malaysia', 105.3, 7, 80000, 'male', "https://images.dog.ceo/breeds/african/n02116738_2988.jpg", NULL);
+insert into pets values (NULL, 'Leo', 'tiger', 'malaysia', 105.3, 5, 100000, 'male', "https://images.dog.ceo/breeds/african/n02116738_2988.jpg", NULL);
