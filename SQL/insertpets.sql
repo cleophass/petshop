8efc360974613347d540506087db3a30c6b193f8
StritanS <tristan.duong36@hotmail.fr>
2022-12-08 08:04:48 +0100

    correction de nom

--- SQL/insertpets.sql
+++ SQL/insertpets.sql
@@ -1,7 +1,7 @@
 use datapets;
 
-insert into pets values (NULL, 'Milo', 'Chien', 'Berger Allemand', 20, 2, 1000, 'Male', "https://images.dog.ceo/breeds/african/n02116738_2988.jpg", NULL);
-insert into pets values (NULL, 'Patty', 'Chien', 'Bouvier Bernois', 30, 4, 2000, 'Female', "https://www.sciencesetavenir.fr/assets/img/2022/05/11/cover-r4x3w1000-627b878098579-specificites-du-bouvier-bernois.jpg", NULL);
+insert into pets values (NULL, 'Milo', 'Chien', 'Berger_Allemand', 20, 2, 1000, 'Male', "https://images.dog.ceo/breeds/african/n02116738_2988.jpg", NULL);
+insert into pets values (NULL, 'Patty', 'Chien', 'Bouvier_Bernois', 30, 4, 2000, 'Female', "https://www.sciencesetavenir.fr/assets/img/2022/05/11/cover-r4x3w1000-627b878098579-specificites-du-bouvier-bernois.jpg", NULL);
 insert into pets values (NULL, 'Bella', 'Chien', 'Chihuahua', 5, 1, 500, 'Female', "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Chihuahua_portrait.jpg/1200px-Chihuahua_portrait.jpg", NULL);
 insert into pets values (NULL, 'Tiger', 'Chat', 'Siamois', 5, 2, 500, 'Male', "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJvW9-B_nFFW8ifJ40fSIeVGl5YH4Mxu_b9g&usqp=CAU", NULL);
 insert into pets values (NULL, 'Luna', 'Chat', 'Siamois', 5, 2, 500, 'Female', "https://images.dog.ceo/breeds/african/n02116738_2988.jpg", NULL);
